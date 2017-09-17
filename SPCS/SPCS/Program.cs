8cb09d9ca88c5452c04c2cf09c8bfdb0aef4e8ae
mukatk <mukatk@gmail.com>
2017-09-17 14:56:55 -0300

    Adicionando referência do LZW

--- SPCS/SPCS/Program.cs
+++ SPCS/SPCS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpLZW;
 
 namespace SPCS
 {
@@ -10,6 +11,7 @@ namespace SPCS
     {
         public static void Main(string[] args)
         {
+            
         }
     }
 }
