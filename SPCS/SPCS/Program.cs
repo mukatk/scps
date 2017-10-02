36636a4ae68eea8b0091b24c710e221a45e5059f
mukatk <mukatk@gmail.com>
2017-10-01 22:53:25 -0300

    Ajustando nome CSPROJ e Solution

--- SPCS/SPCS/Program.cs
+++ SPCS/SPCS/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SharpLZW;
-using SPCS.Util;
+using SCPS.Util;
 
-namespace SPCS
+namespace SCPS
 {
     public class Program
     {
--- SPCS/SPCS/Util/Huffman.cs
+++ SPCS/SPCS/Util/Huffman.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace SPCS.Util
+namespace SCPS.Util
 {
     public class HuffmanTree
     {
